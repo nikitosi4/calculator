b29d10646bc94742b9accfb8a99ebac02aa34697
nikitosi4 <kitvladimirovi4@gmail.com>
2023-04-05 17:02:11 +0300

    adds point, change, and bracket buttons

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ import tkmacosx as tk
 
 def add_digit(digit):
     value = calc.get()
-    if value[0] == "0":
+    if value[0] == "0" and len(value) == 1:
         value = value[1:]
     calc.delete(0, END)
     calc.insert(0, value+str(digit))
@@ -42,6 +42,37 @@ def del_last():
     calc.insert(0, value)
 
 
+def bracket():
+    value = calc.get()
+    if value[0] == "0":
+        value = value[1:]
+        value = value + "("
+    elif value[-1] in "0123456789":
+        value = value + ")"
+    else:
+        value = value + "("
+    calc.delete(0, END)
+    calc.insert(0, value)
+
+
+def point():
+    value = calc.get()
+    if value[-1] in "0123456789":
+        value = value + "."
+    calc.delete(0, END)
+    calc.insert(0, value)
+
+
+def change():
+    value = calc.get()
+    if value[0] == "-":
+        value = value[1:]
+    else:
+        value = "-" + value
+    calc.delete(0, END)
+    calc.insert(0, value)
+
+
 def make_button_dig(digit):
     return tk.Button(text=f"{digit}", bd=4, command=lambda: add_digit(digit))
 
@@ -74,15 +105,15 @@ for row in range(3):
 
 tk.Button(win, text="⌫", bd=4, command=del_last).grid(row=0, column=3, padx=3, pady=3)
 tk.Button(win, text="C", bd=4, command=del_all).grid(row=1, column=0, padx=3, pady=3)
-tk.Button(win, text="()", bd=4).grid(row=1, column=1, padx=3, pady=3)
+tk.Button(win, text="()", bd=4, command=bracket).grid(row=1, column=1, padx=3, pady=3)
 tk.Button(win, text="%", bd=4).grid(row=1, column=2, padx=3, pady=3)
 make_button_op("/").grid(row=1, column=3, padx=3, pady=3)
 make_button_op("*").grid(row=2, column=3, padx=3, pady=3)
 make_button_op("-").grid(row=3, column=3, padx=3, pady=3)
 make_button_op("+").grid(row=4, column=3, padx=3, pady=3)
-tk.Button(win, text="+-", bd=4).grid(row=5, column=0, padx=3, pady=3)
+tk.Button(win, text="+-", bd=4, command=change).grid(row=5, column=0, padx=3, pady=3)
 make_button_dig("0").grid(row=5, column=1, padx=3, pady=3)
-tk.Button(win, text=".", bd=4).grid(row=5, column=2, padx=3, pady=3)
+tk.Button(win, text=".", bd=4, command=point).grid(row=5, column=2, padx=3, pady=3)
 make_button_eq("=").grid(row=5, column=3, padx=3, pady=3)
 
 
